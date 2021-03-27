fd2dd57ac7ede9b2fdf609e94a3fa3c35deddb48
Raihan Iyai <raihan.suraperwata@tokopedia.com>
2021-03-28 01:18:46 +0700

    update query company

--- src/feature/company.js
+++ src/feature/company.js
@@ -1,5 +1,5 @@
 const query = `
-  SELECT p.id, p.name, p.price, p.duration_type from plan p
+  SELECT p.id, p.name, p.price, p.duration_type, p.type from plan p
   JOIN service s ON s.id = p.service_id
   WHERE s.id = $1
   ORDER BY p.id
